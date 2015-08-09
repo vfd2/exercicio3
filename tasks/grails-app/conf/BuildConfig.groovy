300d9ec9e29ff2e5ebd0652d5aff6f7036496726
vfd2 <facundes@gmail.com>
2015-08-09 17:42:46 -0300

    alteracao buildconf

--- tasks/grails-app/conf/BuildConfig.groovy
+++ tasks/grails-app/conf/BuildConfig.groovy
@@ -51,8 +51,6 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
-		compile ":heroku:1.0.1"
-		runtime 'postgresql:postgresql:8.4-702.jdbc3'
     }
 
     plugins {
@@ -63,8 +61,6 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:2.1.5"
-		compile ':heroku:1.0.1'
-		compile ':cloudsupport:1.0.11'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"

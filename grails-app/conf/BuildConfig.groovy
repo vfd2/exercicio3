09c48cb73b1e758fd6d2d3590ceebfe3917392d0
vfd2 <facundes@gmail.com>
2015-08-09 18:20:02 -0300

    alteracao no buildconfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,7 +51,7 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
-		runtime 'postgresql:postgresql:9.1-901-1.jdbc4'
+		runtime 'postgresql:postgresql:8.4-702.jdbc3'
 		
     }
 
